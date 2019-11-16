1fe6d7b0a01f48740810b0863cf45d7f8b68ab57
BlueLort <emaranasr@gmail.com>
2019-11-16 05:05:13 +0200

    adding some functionality to the class to use it for model rendering

--- Game X Engine/src/Object/GXMeshObject/GXModelObject.cpp
+++ Game X Engine/src/Object/GXMeshObject/GXModelObject.cpp
@@ -7,7 +7,17 @@ namespace gx {
 
 
 	void GXModelObject::init(const char* filePath, const char* shaderPath) {
-
+	
+		//TODO SOLVE AND READ THE MODEL
+		//io::IOManager::GLAssimpRead(filePath, meshRenderIDs);
+		if (shaderPath == nullptr) {
+	#ifdef  USING_OPENGL
+			shader=GLShaderManager::getShader(GLShaderType::DEFAULT);
+		}
+		else {
+			shader=GLShaderManager::getShader(shaderPath);
+	#endif
+		}
 	}
 
 	void GXModelObject::update(float deltaTime) {
@@ -18,7 +28,7 @@ namespace gx {
 
 	void GXModelObject::destroy() {
 		for (int i = 0; i < meshRenderIDs.size(); i++) {
-			meshRenderIDs[i].destroy();
+			meshRenderIDs[i]->destroy();
 		}
 	}
 
--- Game X Engine/src/Object/GXMeshObject/GXModelObject.h
+++ Game X Engine/src/Object/GXMeshObject/GXModelObject.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "../GXObject.h"
 #include "Renderer/openGL/Buffers/GLBufferManager.h"
+//#include "IOManagement/IOManager.h"
 namespace gx {
 	//Model Object is Object with RendererComponent and Assimp Loaded Object.
 	class GX_DLL GXModelObject : public GXObject {
@@ -8,11 +9,16 @@ namespace gx {
 		virtual void init(const char* filePath,const char* shaderPath=nullptr);
 		virtual void update(float deltaTime) override;
 		virtual void destroy() override;
+
+		virtual inline void GLDraw() override{
+			//TODO SUBMIT INFORMATION TO THE RENDERER TO RENDER THEM
+		}
+
 	private:
 
 #ifdef USING_OPENGL
-		std::vector<GLBufferManager> meshRenderIDs;
-		GLShader shader;
+		std::vector<std::shared_ptr<GLBufferManager>> meshRenderIDs;
+		GLShader* shader;
 #endif
 
 	};
