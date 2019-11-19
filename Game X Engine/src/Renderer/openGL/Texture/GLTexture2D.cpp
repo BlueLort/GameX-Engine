0066126de125e3a9efaccce2dbea51ac41637b0b
BlueLort <emaranasr@gmail.com>
2019-11-20 01:56:42 +0200

    moving init implementation to source file

---
+++ Game X Engine/src/Renderer/openGL/Texture/GLTexture2D.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "GLTexture2D.h"
+
+
+namespace gx {
+
+
+
+	void GLTexture2D::init(const uint8_t* data, int32_t width, int32_t height, GLTexture2DType Type) {
+
+		glGenTextures(1, &ID);
+		glBindTexture(GL_TEXTURE_2D, ID);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+
+}
