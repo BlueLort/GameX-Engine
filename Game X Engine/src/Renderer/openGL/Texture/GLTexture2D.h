b413f0514799b41f155e858ea5afe1a0e9f368e3
BlueLort <emaranasr@gmail.com>
2019-11-20 02:02:37 +0200

    moving initialization to source file and adding getter for ID

--- Game X Engine/src/Renderer/openGL/Texture/GLTexture2D.h
+++ Game X Engine/src/Renderer/openGL/Texture/GLTexture2D.h
@@ -15,25 +15,13 @@ namespace gx {
 	class GX_DLL GLTexture2D
 	{
 	public:
-		inline void use() {
-			glBindTexture(GL_TEXTURE_2D,ID);
-		}
 		inline void init(GLuint id, GLTexture2DType Type) {
 			ID = id;
 			type = Type;
 		}
-		inline void init(const uint8_t* data,int32_t width,int32_t height,GLTexture2DType Type){
-		
-			glGenTextures(1, &ID);
-			glBindTexture(GL_TEXTURE_2D, ID);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-			glGenerateMipmap(GL_TEXTURE_2D);
-
-			glBindTexture(GL_TEXTURE_2D, 0);
+		void init(const uint8_t* data, int32_t width, int32_t height, GLTexture2DType Type);
+		inline void use() {
+			glBindTexture(GL_TEXTURE_2D,ID);
 		}
 		inline static void stop() {
 			glBindTexture(GL_TEXTURE_2D, 0);
@@ -42,7 +30,7 @@ namespace gx {
 
 			glDeleteTextures(1, &ID);
 		}
-
+		inline GLuint getID()const { return ID; }
 	private:
 		GLuint ID;
 		GLTexture2DType type;
