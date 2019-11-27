37758e72bf9ce6d07efa7be6fc4362df6278608a
BlueLort <emaranasr@gmail.com>
2019-11-27 02:48:08 +0200

    GLDraw Logic using the ShaderLibrary and GLBufferManager

--- Game X Engine/src/Object/GXMeshObject/GXModelObject.cpp
+++ Game X Engine/src/Object/GXMeshObject/GXModelObject.cpp
@@ -31,10 +31,51 @@ namespace gx {
 	{
 
 
+
 	}
 	void GXModelObject::GLDraw()
 	{
+		this->glshader->use();
+		for (auto ite : meshRenderIDs) {
+			uint32_t nDiffuse = 0;
+			uint32_t nSpecular = 0;
+			auto textures = ite->getTextures();
+			for (uint32_t i = 0; i < textures.size(); i++)
+			{
+				GLTexture2D::setActiveTexture(i);
+				GLTexture2DType type = textures[i]->getType();
+				std::string name;
+				switch (type)
+				{
+				case gx::DIFFUSE:
+					name="material.diffuse"+std::to_string(nDiffuse++);
+					break;
+				case gx::SPECULAR:
+					name = "material.specular" + std::to_string(nSpecular++);
+					break;
+				case gx::SPECULAR_MAP:
+					break;
+				case gx::NORMAL:
+					break;
+				case gx::HEIGHT:
+					break;
+				case gx::AMBIENT:
+					break;
+				default:
 
+					break;
+				}
+				this->glshader->setFloat(name.c_str(), i);
+				textures[i]->use();
+			}
+			GLTexture2D::setActiveTexture(0);
+
+			// draw mesh
+			ite->use();
+			GLRenderer::getInstance().draw(ite->getNumberOfElements(), RenderType::GX_TRIANGLES);
+			GLBufferManager::stop();
+		}
+		
 
 	}
 
